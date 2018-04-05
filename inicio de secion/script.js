a53c0796c38337c0cc85cdd78a478bb19e78725b
mabelsit <35733946+mabelsit@users.noreply.github.com>
2018-04-05 12:28:40 -0600

    Add files via upload

---
+++ inicio de secion/script.js
@@ -0,0 +1,10 @@
+var myweb = angular.module("myweb",[]);
+ myweb.controller("Login", function ($scope){
+ 	//funcion Inicio de sesion
+
+$scope.Inicio = function(){
+	alert("ya me puedo graduar")
+
+}
+
+});
